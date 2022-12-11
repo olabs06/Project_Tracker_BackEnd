217cc9942a4e115fa01b6ca295bffb6f8b775d9f
olabs06 <olabs06@yahoo.com>
2022-12-11 16:44:02 +0100

    merging front-end with back-end

---
+++ client/src/components/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const About= ()=>{
+
+    window.location.replace('https://moringaschool.com/about-us/');
+    return null;
+}
+
+export default About;
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_12_07_172345) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "projects", force: :cascade do |t|
     t.string "title"
     t.integer "cohort"
